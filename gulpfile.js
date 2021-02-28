d3806f8030a661cb3dcd2f9564c56d5d3dc65016
Gustavo <gustavoapereirau@gmail.com>
2021-02-28 09:38:30 -0500

    Se agregaron imagenes webp e imagenes minificadas

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,51 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify  = require('gulp-notify');
+const webp = require('gulp-webp');
+
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+}
+
 
 //Funcion que compila SASS
 
 function css () {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass() )
         .pipe(dest('./build/css') )
 }
 
 function minificarcss () {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: 'compressed'
         }) )
         .pipe(dest('./build/css') )
 }
 
+function imagen(){
+    return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest('./build/img/'))
+    .pipe(notify( {message: 'Imagen minificada'}))
+}
+
+function versionwebp(){
+    return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest('./build/img/'))
+    .pipe(notify( {message: 'Version Webp lista'}))
+}
+
 function watchArchivos(){
-    watch('src/scss/**/*.scss', css) //* = Todas los archivos de esta carpeta - ** = Todos los archivos de todas las carpetas
+    watch(paths.scss, css) //* = Todas los archivos de esta carpeta - ** = Todos los archivos de todas las carpetas
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series( css, imagen,versionwebp, watchArchivos);
